6716844604652e93f98eb4f65cde3b1e5cb142ea
michaelwro <michael.wrona77@gmail.com>
2023-04-01 05:43:34 +0000

    Made utility functions template-only

--- include/asin_safe.h
+++ include/asin_safe.h
@@ -7,6 +7,12 @@
 #ifndef MATHUTILS_ASIN_SAFE_H_
 #define MATHUTILS_ASIN_SAFE_H_
 
+#include "constants.h"
+
+#include <cmath>
+#include <cassert>
+#include <type_traits>
+
 namespace MathUtils {
 
 /**
@@ -15,10 +21,27 @@ namespace MathUtils {
  * @details If the input is greater than 1, pi/2 is returned. If the input is less than -1, -pi/2
  * is returned.
  *
+ * @tparam T Input type.
  * @param val Value to take the arcsine of, in [rad].
  * @return Arcsine of `val`.
  */
-double asin_safe(const double val) noexcept;
+template<typename T>
+double asin_safe(const T val) noexcept
+{
+    static_assert(std::is_fundamental<T>::value, "Fundamental types only.");
+    assert(std::abs(val) <= 1.0);
+
+    const double dval = static_cast<double>(val);
+
+    if (dval >= 1.0) {
+        return Constants::PI_DIV2;
+    }
+    else if (dval <= -1.0) {
+        return -Constants::PI_DIV2;
+    }
+
+    return std::asin(dval);
+}
 
 }  // MathUtils
 
