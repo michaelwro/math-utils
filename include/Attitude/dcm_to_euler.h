a3b02d01b136db6eed87b5fc3eee4b3b1c05df11
michaelwro <michael.wrona77@gmail.com>
2023-04-02 17:49:32 +0000

    Created DCM to Euler 321 function.

---
+++ include/Attitude/dcm_to_euler.h
@@ -0,0 +1,29 @@
+/**
+ * @file dcm_to_euler.h
+ * @author Michael Wrona
+ * @date 2023-04-02
+ */
+
+#ifndef MATHUTILS_DCM_TO_EULER_H_
+#define MATHUTILS_DCM_TO_EULER_H_
+
+#include "Attitude/Euler321.h"
+#include "LinAlg/Matrix.h"
+
+namespace MathUtils {
+
+/**
+ * @brief Convert a direction cosine matrix to 321 Euler angles.
+ *
+ * @details Equations from "Analytical Mechanics of Aerospace Systems" (Schaub).
+ *
+ * @param dcm DCM to convert.
+ * @return Corresponding 321 Euler angles.
+ *
+ * @ref https://www.vectornav.com/resources/inertial-navigation-primer/math-fundamentals/math-attitudetran
+ */
+Euler321 dcm_to_euler(const Matrix<3,3>& dcm);
+
+}  // namespace MathUtils
+
+#endif  // MATHUTILS_DCM_TO_EULER_H_
